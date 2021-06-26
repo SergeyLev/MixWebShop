8da746178d7d573ebc52d569feba32e9134ac4fb
2dt <davis@teivens.lv>
2021-06-26 13:50:40 +0300

    add Person and ShippingAddress models in models.py

--- MixWebShop/models.py
+++ MixWebShop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.TextField(max_length=100, null=False)
+    surname = models.TextField(max_length=100, null=False)
+    date_of_birth = models.DateField(max_length=10)
+
+class ShippingAddress(models.Model):
+    name = models.ForeignKey(Person, null=False, on_delete=models.DO_NOTHING)
+    surname = models.ForeignKey(Person, null=False, on_delete=models.CASCADE)
+    country = models.TextField(max_length=100, null=False)
+    city = models.TextField(max_length=50, null=False)
+    zip_code = models.TextField(max_length=20, null=False)
+    phone = models.IntegerField(blank=True)

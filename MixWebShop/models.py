6b3872f0217ef4ed041b55d1f12a7171682c52a1
SergeyLev <s.levsov@gmail.com>
2021-06-26 14:58:29 +0300

    Models change

--- MixWebShop/models.py
+++ MixWebShop/models.py
@@ -1,26 +1,40 @@
 from django.db import models
 
-class Person(models.Model):
-    name = models.TextField(max_length=100, null=False)
-    surname = models.TextField(max_length=100, null=False)
-    date_of_birth = models.DateField(max_length=10)
-    # Admin vai User?
 
 class ShippingAddress(models.Model):
-    name = models.ForeignKey(Person, null=False, on_delete=models.DO_NOTHING) # Kads seit datu tips?
-    surname = models.ForeignKey(Person, null=False, on_delete=models.CASCADE) # Kads seit datu tips?
-    # https://docs.djangoproject.com/en/3.2/topics/db/examples/many_to_one/
-    
+    address_id = models.AutoField(primary_key=True)  # Primary key
     country = models.TextField(max_length=100, null=False)
     city = models.TextField(max_length=50, null=False)
     zip_code = models.TextField(max_length=20, null=False)
     phone = models.IntegerField(blank=True)
 
+
+class Person(models.Model):
+    person_id = models.AutoField(primary_key=True)  # Primary key
+    name = models.TextField(max_length=100, null=False)
+    surname = models.TextField(max_length=100, null=False)
+    date_of_birth = models.DateField(max_length=10)
+
+
+class Admin(models.Model):
+    admin_id = models.AutoField(primary_key=True)  # Primary key
+    person_id = models.ForeignKey(to=Person.person_id, on_delete=models.CASCADE)  # Foreign Key
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)  # Primary key
+    person_id = models.ForeignKey(to=Person.person_id, on_delete=models.CASCADE)  # Foreign Key
+    address_id = models.ForeignKey(to=ShippingAddress.address_id, on_delete=models.CASCADE)  # Foreign Key
+
+
 class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)  # Primary key
     name = models.CharField(max_length=150, null=False)
 
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_id = models.AutoField(primary_key=True)  # Primary key
+    category_id = models.ForeignKey(Category.category_id, on_delete=models.CASCADE)  # Foreign Key
     name = models.TextField(max_length=100, null=False)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)

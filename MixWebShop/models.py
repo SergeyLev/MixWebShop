2abf6e130ee5f6a24aca4f58a07644fb4069d03a
SergeyLev <36793484+SergeyLev@users.noreply.github.com>
2021-06-26 14:14:42 +0300

    Update models.py
    Comments

--- MixWebShop/models.py
+++ MixWebShop/models.py
@@ -4,10 +4,13 @@ class Person(models.Model):
     name = models.TextField(max_length=100, null=False)
     surname = models.TextField(max_length=100, null=False)
     date_of_birth = models.DateField(max_length=10)
+    # Admin vai User?
 
 class ShippingAddress(models.Model):
-    name = models.ForeignKey(Person, null=False, on_delete=models.DO_NOTHING)
-    surname = models.ForeignKey(Person, null=False, on_delete=models.CASCADE)
+    name = models.ForeignKey(Person, null=False, on_delete=models.DO_NOTHING) # Kads seit datu tips?
+    surname = models.ForeignKey(Person, null=False, on_delete=models.CASCADE) # Kads seit datu tips?
+    # https://docs.djangoproject.com/en/3.2/topics/db/examples/many_to_one/
+    
     country = models.TextField(max_length=100, null=False)
     city = models.TextField(max_length=50, null=False)
     zip_code = models.TextField(max_length=20, null=False)

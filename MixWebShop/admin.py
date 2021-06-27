490d20df38c48bae0ffc944866c65c32c12352ca
2dt <davis@teivens.lv>
2021-06-27 10:27:56 +0300

    edit models.py, register models in admin.py , add ShippingAddress views in views.py

--- MixWebShop/admin.py
+++ MixWebShop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ShippingAddress, Person, Product, Admin, Category, User
 
-# Register your models here.
+admin.site.register(ShippingAddress)
+admin.site.register(Person)
+admin.site.register(Product)
+admin.site.register(Admin)
+admin.site.register(Category)
+admin.site.register(User)
--- MixWebShop/views.py
+++ MixWebShop/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import ShippingAddress, Person, Product, Admin, Category, User
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, DeleteView, CreateView, UpdateView
 
-# Create your views here.
+
+class ShippingAddress(DetailView):
+    template_name = 'shipping_address.html'
+    model = ShippingAddress
+    context_object_name = 'shipping_adress'
+
+class CreateShippingAddress(CreateView):
+    template_name = 'create_shipping_address.html'
+    model = ShippingAddress
+    success_url = reverse_lazy('shipping_address')
+    fields = '__all__'
+
+class UpdateShippingAddress(UpdateView):
+    template_name = 'update_shipping_address.html'
+    model = ShippingAddress
+    success_url = reverse_lazy('shipping_address')
+    fields = '__all__'
+    context_object_name = 'address'
+
+class DeleteShippingAddress(DeleteView):
+    template_name = 'delete_shipping_address.html'
+    model = ShippingAddress
+    context_object_name = 'address'
+    success_url = reverse_lazy('shipping_address')
